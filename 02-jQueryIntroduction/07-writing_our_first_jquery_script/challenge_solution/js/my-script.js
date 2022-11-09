3c973d58212616f210cc8d1792520dc5f1f4e1d8
MinuMThomas <minumarytomas@gmail.com>
2022-11-09 17:36:58 +0000

    add jquery file

---
+++ 02-jQueryIntroduction/07-writing_our_first_jquery_script/challenge_solution/js/my-script.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//     $("h2").css("text-decoration", "undrline"); //underline all h2 elements
+//     $("ul").css("border", "solid 1px #ccc"); //adds border to all lists
+// });
+    
+
+        // $(document).ready(function(){
+        //     $("h2").addClass("undrline"); //underline all h2 elements
+        //     $("ul").addClass("border"); //adds border to all lists
+        // });
+
+
+
+        $(document).ready(function(){
+            $("h2").removeClass("undrline"); //underline all h2 elements
+            $("ul").removeClass("border"); //adds border to all lists
+        });
